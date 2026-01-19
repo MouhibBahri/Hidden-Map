e72095451407a6b828d6085173945eb5a99ebc18
mahjoubr <152096395+mahjoubr@users.noreply.github.com>
2026-01-19 13:05:00 +0100

    mobile: configured CORS in the backend to accept all origins

--- mobile/lib/config/constants.dart
+++ mobile/lib/config/constants.dart
@@ -1,7 +1,7 @@
 class ApiConstants {
   // Use 10.0.2.2 for Android emulator to access localhost
-  static const String baseUrl = 'http://10.0.2.2:3000';
-  // static const String baseUrl = 'http://localhost:3000';
+  //static const String baseUrl = 'http://10.0.2.2:3000';
+   static const String baseUrl = 'http://localhost:3000';
   static const String locationsEndpoint = '/locations';
   static const String leaderboardEndpoint = '/leaderboard';
   static const String adminPendingEndpoint = '/admin/pending-locations';
